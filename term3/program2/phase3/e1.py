513cda6517c06c88bdbbad810b88aa369abbd0ea
AsalEmrani <asal.s.emrani@gmail.com>
2024-12-18 15:09:27 +0330

    guide block for maze

---
+++ term3/program2/phase3/e1.py
@@ -0,0 +1,14 @@
+import collections
+from random import *
+from time import *
+
+from mcpi_e.minecraft import *
+from mcpi_e.block import *
+collections.Iterable = collections.abc.Iterable
+server_address = "Pycraft.yasan.ac"
+python_port = 11130
+player_name = "a_emrani"
+
+mc = Minecraft.create(server_address, python_port, player_name)
+
+x, y, z = mc.player.getTilePos() 
